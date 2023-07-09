12018bf3987d79f3af9ac0ee3c66d851d0def65f
eduardoCarazas <117487902+eduardoCarazas@users.noreply.github.com>
2023-07-09 00:15:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 st.title('Exploratory Data Analysis of the Iris Dataset')
-st.header('This app allows you to explore the Iris dataset and visualize the data using various plots.')
