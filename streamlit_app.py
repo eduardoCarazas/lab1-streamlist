389edb1f7094447a35a12a83f98ac2366cefe8f5
eduardoCarazas <117487902+eduardoCarazas@users.noreply.github.com>
2023-07-10 16:54:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,16 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+# Loading dataset 
+df = pd.read_csv('https://raw.githubusercontent.com/eduardoCarazas/lab1-streamlist/main/Lista%20proveedores%202023_10.07.2023.csv')
+
 st.title('Exploratory Data Analysis of the Iris Dataset')
+st.header('This app allows you to explore the Iris dataset and visualize the data using various plots.')
+
+st.subheader("DataSet")
+st.dataframe(df)
+selected_column = st.sidebar.selectbox('Select a column to visualize', df.columns)
+
+st.write("Histograma")
+sns.histplot(df[selected_column])
+st.pyplot()
