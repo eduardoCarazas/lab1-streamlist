0df17aad78dec40da6b06f3327a5219a50f29c1b
eduardoCarazas <117487902+eduardoCarazas@users.noreply.github.com>
2023-07-10 17:39:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,15 @@ ssl._create_default_https_context = ssl._create_unverified_context
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Loading dataset 
-df = pd.read_csv('https://raw.githubusercontent.com/eduardoCarazas/lab1-streamlist/main/Casos%20de%20Anemia%20(Cuzco)%202010-2020.csv')
+# df = pd.read_csv('https://raw.githubusercontent.com/eduardoCarazas/lab1-streamlist/main/Casos%20de%20Anemia%20(Cuzco)%202010-2020.csv')
 
 st.title('Exploratory Data Analysis of the Iris Dataset')
 st.header('This app allows you to explore the Iris dataset and visualize the data using various plots.')
 
 st.subheader("DataSet")
-st.dataframe(df)
-selected_column = st.sidebar.selectbox('Select a column to visualize', df.columns)
+# st.dataframe(df)
+# selected_column = st.sidebar.selectbox('Select a column to visualize', df.columns)
 
 st.write("Histograma")
-sns.histplot(df[selected_column])
-st.pyplot()
+# sns.histplot(df[selected_column])
+# st.pyplot()
